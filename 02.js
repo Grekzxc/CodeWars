4e277686bc95c9fd2dcf10c2508a8c34ed30e724
Grekzxc <cool.grek456852@yandex.by>
2023-02-08 23:29:06 +0300

    CodeWars

---
+++ 02.js
@@ -0,0 +1,5 @@
+// String repeat (Повтор строки)
+function repeatStr (n, s) {
+    return s.repeat(n);
+  }
+//   Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
