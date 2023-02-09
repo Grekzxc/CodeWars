5c7488f1410bf41e60f156e4ff691ce846bcab44
Grekzxc <cool.grek456852@yandex.by>
2023-02-09 11:32:39 +0300

    CodeWars

---
+++ 21.js
@@ -0,0 +1,9 @@
+// Invert values
+
+
+function invert(array) {
+    for(let i = 0 ; i < array.length; i++){
+       array[i] = array[i] * (-1) 
+     }
+     return array
+   }
---
+++ 24.js
@@ -0,0 +1,9 @@
+// Double Char
+
+function doubleChar(str) {
+    var newStr = '';
+   for (var i = 0; i < str.length; i++){
+     newStr += str[i] + str[i];
+   }
+   return newStr;
+ };
